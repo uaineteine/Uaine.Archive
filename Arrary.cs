df6d4a6a90117241e4399e47a9393e0f94b5bb33
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2024-01-17 21:48:42 +1100

    archive type array added

---
+++ Arrary.cs
@@ -0,0 +1,23 @@
+﻿using Uaine.IO;
+
+namespace Uaine.Archive
+{
+    public class Arrary<T>
+    {
+        public T[] Items { get; private set; }
+        public int Size { get => Items.Length; }
+
+        public Arrary(string[] json)
+        {
+            Items = ArraySerialiser.DeserializeFromJson<T>(json);
+        }
+        public Arrary(T[] items)
+        {
+            Items = items;
+        }
+        public string[] SerialiseToJSON()
+        {
+            return ArraySerialiser.SerializeToJson<T>(Items);
+        }
+    }
+}
