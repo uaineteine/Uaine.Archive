9892bbda3b86f6b017647a91bbafb88250d94c5c
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2024-01-17 21:38:43 +1100

    json array deserialise method

--- JsonArrary.cs
+++ JsonArrary.cs
@@ -1,7 +1,15 @@
-﻿namespace Uaine.Archive
-{
-    public class JsonArrary
-    {
+﻿using Uaine.IO;
 
+namespace Uaine.Archive
+{
+    public class JsonArrary<T>
+    {
+        public T[] Items { get; private set; }
+        public int Size { get => Items.Length; }
+
+        public JsonArrary(string[] json)
+        {
+            Items = ArraySerialiser.DeserializeFromJson<T>(json);
+        }
     }
 }
