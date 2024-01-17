1e4285a68f7bf0eb559ff818ce389b8e2e09106e
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2024-01-17 22:23:47 +1100

    better constructor use of both classes

---
+++ JsonFile.cs
@@ -0,0 +1,18 @@
+﻿using Uaine.IO;
+
+namespace Uaine.Archive
+{
+    public static class JsonFileType
+    {
+        public const int Object = 0;
+        public const int Array = 1;
+    }
+    public class JsonFile : FileData
+    {
+        public int Type { get; private set; }
+        public JsonFile(string filename, int type) : base(filename)
+        {
+            Type = type;
+        }
+    }
+}
