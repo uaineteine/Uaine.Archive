27f8147b94a207c88af658bf4c5f87f8f12eada9
Daniel <39141263+uaineteine@users.noreply.github.com>
2024-01-20 01:32:06 +1100

    add dataname to jsonfile

--- JsonFileStore.cs
+++ JsonFileStore.cs
@@ -5,16 +5,19 @@ namespace Uaine.Archive
     public class JsonFileStore<T> : FileData
     {
         public int Type { get; private set; }
+        public string DataName { get; private set; }
         public DataArray<T> Dataarray { get; private set; }
 
-        public JsonFileStore(string filename) : base(filename)
+        public JsonFileStore(string filename, string dataname) : base(filename)
         {
+            DataName = dataname;
             LoadAllLines();
             Dataarray = new DataArray<T>(Lines);
         }
 
-        public JsonFileStore(string filname, T[] items) : base(filname)
+        public JsonFileStore(string filname, string dataname, T[] items) : base(filname)
         {
+            DataName = dataname;
             Dataarray = new DataArray<T>(items);
             Lines = Dataarray.SerialiseToJSON();
             SaveAllLines();
