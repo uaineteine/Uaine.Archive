2954976da9b0665cdaa2823e5bf2485fb0dc8224
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2024-01-20 00:29:39 +1100

    able to now save json files and load them as an array type automatically

---
+++ JsonFileStore.cs
@@ -0,0 +1,23 @@
+﻿using Uaine.IO;
+
+namespace Uaine.Archive
+{
+    public class JsonFileStore<T> : FileData
+    {
+        public int Type { get; private set; }
+        public DataArray<T> Dataarray { get; private set; }
+
+        public JsonFileStore(string filename) : base(filename)
+        {
+            LoadAllLines();
+            Dataarray = new DataArray<T>(Lines);
+        }
+
+        public JsonFileStore(string filname, T[] items) : base(filname)
+        {
+            Dataarray = new DataArray<T>(items);
+            Lines = Dataarray.SerialiseToJSON();
+            SaveAllLines();
+        }
+    }
+}
